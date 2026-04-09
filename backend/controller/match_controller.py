f5358f2ffa74547c20433f146df06a57a69f60e0
YX14ng <mguan@fi.uba.ar>
2026-04-09 15:41:56 -0300

    Terminar el Por completo el GET

--- backend/controller/match_controller.py
+++ backend/controller/match_controller.py
@@ -1,23 +1,36 @@
 from backend.database.database import conectar_db
 
-def mostrar_partidos(equipo, fecha, fase):
+def mostrar_partidos(equipo, fecha, fase, limit, offset):
     conn = conectar_db()
     cursor = conn.cursor()
-    sql = "SELECT * FROM partidos WHERE 1=1" 
+    sql = "SELECT * FROM partidos"
+    condition = " WHERE 1=1" 
+    pagination = " "
     params = []
 
     if fecha:
-        sql += " AND fecha = %s"
+        condition += " AND fecha = %s"
         params.append(fecha)
     if equipo:
-        sql += " AND (equipo_local = %s OR equipo_visitante = %s)"
+        condition += " AND (equipo_local = %s OR equipo_visitante = %s)"
         params.extend([equipo, equipo])
     if fase:
-        sql += " AND fase = %s"
+        condition += " AND fase = %s"
         params.append(fase)
     
-    cursor.execute(sql, params)
+    cursor.execute("SELECT COUNT(*) as total FROM partidos " + condition, params)
+    total = cursor.fetchone()[0]
+    
+    if limit:
+        pagination += " LIMIT %s"
+        params.append(limit)
+    if offset is not None:
+        pagination += " OFFSET %s"
+        params.append(offset)
+    
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql + condition + pagination, params)
     partidos = cursor.fetchall()
     cursor.close()
     conn.close()
-    return partidos
+    return partidos, total
--- backend/routes/match_route.py
+++ backend/routes/match_route.py
@@ -1,13 +1,25 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, url_for
 from backend.controller.match_controller import mostrar_partidos
 match_bp = Blueprint('match', __name__)
 @match_bp.route("/partidos", methods=["GET"])
-def procesar_partido():
+def listar_partidos():
     equipo = request.args.get('equipo')
     fecha = request.args.get('fecha')
     fase = request.args.get('fase')
-    partidos = mostrar_partidos(equipo, fecha, fase)
-    return jsonify(partidos)
+    limit = request.args.get('_limit', 10,type=int)
+    offset = request.args.get('_offset', 0,type=int)
+    
+    partidos, total = mostrar_partidos(equipo, fecha, fase, limit, offset)
+    
+    last = max(0, (int(total) - 1) // limit) * limit
+    links = {
+        "_first": {"href": url_for('match.listar_partidos', _limit=limit, _offset=0)},
+        "_prev": {"href": url_for('match.listar_partidos', _limit=limit, _offset=max(0, offset - limit))} if offset > 0 else None,
+        "_next": {"href": url_for('match.listar_partidos', _limit=limit, _offset=offset + limit)} if offset + limit < total else None,
+        "_last": {"href": url_for('match.listar_partidos', _limit=limit, _offset=last)}
+    }
+    
+    return jsonify({"partidos": partidos, "_links": links}), 200
 
 
 
